64de79eba3d9e35f25ad157af66b5b84a6155aaf
Clément Doumouro <clement.doumouro@snips.ai>
2017-02-13 23:22:31 +0100

    Use a LimitedSizeDict for the regex parser cache

--- custom_intent_parser/utils.py
+++ custom_intent_parser/utils.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 import os
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -11,3 +13,26 @@ class abstractclassmethod(classmethod):
 
 def sequence_equal(seq, other_seq):
     return len(seq) == len(other_seq) and sorted(seq) == sorted(other_seq)
+
+
+class LimitedSizeDict(OrderedDict):
+    def __init__(self, *args, **kwds):
+        if "size_limit" not in kwds:
+            raise ValueError("'size_limit' must be passed as a keywords "
+                             "argument")
+        self.size_limit = kwds.pop("size_limit")
+        if len(args) > 1:
+            raise TypeError('expected at most 1 arguments, got %d' % len(args))
+        if len(args[0]) + len(kwds) > self.size_limit:
+            raise ValueError("Tried to initialize LimitedSizedDict with more "
+                             "value than permitted with 'limit_size'")
+        super(LimitedSizeDict, self).__init__(*args, **kwds)
+
+    def __setitem__(self, key, value, dict_setitem=OrderedDict.__setitem__):
+        dict_setitem(self, key, value)
+        self._check_size_limit()
+
+    def _check_size_limit(self):
+        if self.size_limit is not None:
+            while len(self) > self.size_limit:
+                self.popitem(last=False)

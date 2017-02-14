bb13601bbc29e73a098d2b6f081d71a2077dbc6c
Clément Doumouro <clement.doumouro@snips.ai>
2017-02-14 19:34:12 +0100

    Fixes for PR

--- custom_intent_parser/result.py
+++ custom_intent_parser/result.py
@@ -1,16 +1,20 @@
-from utils import namedtuple_with_defaults
-
-IntentClassificationResult = namedtuple_with_defaults(
-    "IntentClassificationResult", "name prob")
-
-
 def is_range(rng):
-    if not isinstance(rng, (list, tuple)) or len(rng) != 2:
-        raise ValueError("range must be a length 2 list or tuple")
+    if not isinstance(rng, (list, tuple)) or len(rng) != 2 or rng[0] > rng[1]:
+        raise ValueError("range must be a length 2 list or tuple and must be "
+                         "valid")
+
+
+def classification_result(intent_name=None, prob=None):
+    if intent_name is None:
+        return None
+    res = {"intent": intent_name}
+    if prob is not None:
+        res["prob"] = prob
+    return res
 
 
 def parsed_entity(match_range, value, entity, role=None, **kwargs):
-    parsed_ent = dict()
+    parsed_ent = dict(kwargs)
 
     is_range(match_range)
     parsed_ent["range"] = match_range
@@ -25,26 +29,27 @@ def parsed_entity(match_range, value, entity, role=None, **kwargs):
         assert isinstance(value, (str, unicode))
         parsed_ent["role"] = role
 
-    parsed_ent.update(kwargs)
     return parsed_ent
 
 
-def result(text, intent=None, entities=None):
+def result(text, parsed_intent=None, parsed_entities=None):
     assert isinstance(text, (str, unicode))
-    res = {"text": text, "intent": {}, "entities": []}
+    res = {"text": text, "intent": None, "entities": []}
 
-    if intent is not None:
-        assert isinstance(intent, IntentClassificationResult)
-        if intent.name is not None:
-            res["intent"]["name"] = intent.name
-            if intent.prob is not None:
-                res["intent"]["prob"] = intent.prob
+    if parsed_intent is not None:
+        if parsed_intent["intent"] is None:
+            raise LookupError("'intent' key can't be None if a result is "
+                              "passed")
+        intent = {"name": parsed_intent["intent"]}
+        if parsed_intent["prob"] is not None:
+            intent["prob"] = parsed_intent["prob"]
+        res["intent"] = intent
 
     mandatory_keys = ["range", "value", "entity"]
-    if entities is not None:
-        for ent in entities:
+    if parsed_entities is not None:
+        for ent in parsed_entities:
             for k in mandatory_keys:
                 if k not in ent:
                     raise LookupError("Missing '%s' key" % k)
-    res["entities"] = entities
+    res["entities"] = parsed_entities
     return res
--- custom_intent_parser/tests/test_result.py
+++ custom_intent_parser/tests/test_result.py
@@ -32,9 +32,25 @@ class TestResult(unittest.TestCase):
         text = "a"
         # When/Then
         with self.assertRaises(LookupError) as ctx:
-            result(text, entities=[parsed_ent])
+            result(text, parsed_intent=None, parsed_entities=[parsed_ent])
         self.assertEqual(ctx.exception.message,
                          "Missing 'entity' key")
+
+    def test_result_should_raise_when_no_intent_name_is_none(self):
+        # Given
+        rng = (2, 3)
+        value = "a"
+        parsed_ent = {
+            "range": rng,
+            "value": value
+        }
+        intent = {"intent": None}
+        text = "a"
+        # When/Then
+        with self.assertRaises(LookupError) as ctx:
+            result(text, intent, parsed_entities=[parsed_ent])
+        self.assertEqual(ctx.exception.message,
+                         "'intent' key can't be None if a result is passed")
 
 
 if __name__ == '__main__':

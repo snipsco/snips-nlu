787f3e93c67e6e96ba42993477dcbcf99f6b6324
Clément Doumouro <clement.doumouro@snips.ai>
2017-02-14 16:43:02 +0100

    Refactor results not to use classes

--- custom_intent_parser/result.py
+++ custom_intent_parser/result.py
@@ -1,111 +1,43 @@
 from utils import namedtuple_with_defaults
 
-_IntentClassificationResult = namedtuple_with_defaults(
-    "_IntentClassificationResult", "name prob")
+IntentClassificationResult = namedtuple_with_defaults(
+    "IntentClassificationResult", "name prob")
 
 
-class IntentClassificationResult(_IntentClassificationResult):
-    def to_dict(self):
-        d = {}
-        if self.intent is not None:
-            d["intent"] = self.name
-            if self.intent_prob is not None:
-                d["prob"] = self.prob
-        return d
+def is_range(rng):
+    if not isinstance(rng, (list, tuple)) or len(rng) != 2:
+        raise ValueError("range must be a length 2 list or tuple")
 
 
-class ParsedEntity(object):
-    def __init__(self, match_range, value, entity, role=None, **kwargs):
-        self._range = None
-        self.range = match_range
-        self.value = value
-        self.entity = entity
-        self.role = role
+def parsed_entity(match_range, value, entity, role=None, **kwargs):
+    parsed_ent = dict()
 
-    @property
-    def range(self):
-        return self._range
-
-    @range.setter
-    def range(self, value):
-        if not isinstance(value, (list, tuple)) or len(value) != 2:
-            raise ValueError("range must be a length 2 list or tuple")
-        self._range = list(value)
-
-    def to_dict(self):
-        d = {
-            "range": self.range,
-            "value": self.value,
-            "entity": self.entity
-        }
-        if self.role is not None:
-            d["role"] = self.role
-        return d
+    is_range(match_range)
+    parsed_ent["range"] = match_range
+    parsed_ent["value"] = value
+    parsed_ent["entity"] = entity
+    if role is not None:
+        parsed_ent["role"] = role
+    parsed_ent.update(kwargs)
+    return parsed_ent
 
 
-class EntityExtractionResult(object):
-    def __init__(self, entities=None):
-        self._entities = []
-        self.entities = entities
+def result(text, intent=None, entities=None):
+    assert isinstance(text, (str, unicode))
+    res = {"text": text, "intent": {}, "entities": []}
 
-    @property
-    def entities(self):
-        return self._entities
+    if intent is not None:
+        assert isinstance(intent, IntentClassificationResult)
+        if intent.name is not None:
+            res["intent"]["name"] = intent.name
+            if intent.prob is not None:
+                res["intent"]["prob"] = intent.prob
 
-    @entities.setter
-    def entities(self, values):
-        if not isinstance(values, (list, tuple)):
-            raise ValueError("entities must be a list or tuple of ParsedEntity"
-                             " instance")
-        for e in values:
-            if not isinstance(e, ParsedEntity):
-                raise ValueError("entities are expected to be ParsedEntity "
-                                 "found %s with type %s" % (e, type(e)))
-        self._entities = list(values)
-
-    def as_list(self):
-        return [e.to_dict() for e in self.entities]
-
-
-class Result(object):
-    def __init__(self, text, intent_result=None, entity_results=None):
-        self.text = text
-        self._intent_result = None
-        self._entities_results = None
-        self.intent_results = intent_result
-        self.entities_results = entity_results
-
-    @property
-    def intent_result(self):
-        return self._intent_result
-
-    @intent_result.setter
-    def intent_result(self, value):
-        if not isinstance(value, IntentClassificationResult):
-            raise ValueError("Expected IntentClassificationResult, found: %s"
-                             % type(value))
-
-    @property
-    def entities(self):
-        return self._entities_results
-
-    @entities.setter
-    def entities(self, values):
-        if not isinstance(values, EntityExtractionResult):
-            raise ValueError("entities must be a EntityExtractionResult"
-                             " instances")
-        self._entities_results = values
-
-    def to_dict(self):
-        result = {
-            "text": self.text,
-            "entities": self.entities_results.as_list(),
-            "intent": {}
-        }
-        intent_as_dict = self.intent_result.to_dict()
-        if "intent" in intent_as_dict:
-            result["intent"]["name"] = intent_as_dict["intent"]
-            prob = intent_as_dict.get("prob", False)
-            if prob:
-                result["intent"]["prob"] = prob
-        return result
+    mandatory_keys = ["range", "value", "entity"]
+    if entities is not None:
+        for ent in entities:
+            for k in mandatory_keys:
+                if k not in ent:
+                    raise LookupError("Missing %s key" % k)
+    res["entities"] = entities
+    return res

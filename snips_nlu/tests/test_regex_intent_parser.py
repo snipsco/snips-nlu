2e2e8d2ca323ab447656cdda755ee2e8b018c901
Adrien Ball <adrien.ball@snips.net>
2017-03-20 16:39:21 +0100

    Update tests

--- snips_nlu/tests/test_regex_intent_parser.py
+++ snips_nlu/tests/test_regex_intent_parser.py
@@ -1,126 +1,10 @@
 import os
 import unittest
 
-from snips_nlu.dataset import Dataset
-from snips_nlu.entity import Entity
+from ..dataset import validate_dataset
+from ..result import result, intent_classification_result, parsed_entity
 from snips_nlu.intent_parser.regex_intent_parser import RegexIntentParser
-from snips_nlu.tests.utils import TEST_PATH
-
-
-def get_queries():
-    queries = {
-        "dummy_intent_1": [
-            {
-                "data":
-                    [
-                        {
-                            "text": "This is a "
-                        },
-                        {
-                            "text": "dummy_1",
-                            "entity": "dummy_entity_1",
-                            "slotName": "dummy_slotName"
-                        },
-                        {
-                            "text": " query with another "
-                        },
-                        {
-                            "text": "dummy_2",
-                            "entity": "dummy_entity_2",
-                            "slotName": "dummy_slotName2"
-                        }
-                    ]
-            },
-            {
-                "data":
-                    [
-                        {
-                            "text": "This is another "
-                        },
-                        {
-                            "text": "dummy_2_again",
-                            "entity": "dummy_entity_2",
-                            "slotName": "dummy_slotName2"
-                        },
-                        {
-                            "text": " query."
-                        }
-                    ]
-            },
-            {
-                "data":
-                    [
-                        {
-                            "text": "This is another "
-                        },
-                        {
-                            "text": "dummy_2_again",
-                            "entity": "dummy_entity_2",
-                            "slotName": "dummy_slotName3"
-                        },
-                        {
-                            "text": "?"
-                        }
-                    ]
-            },
-            {
-                "data":
-                    [
-                        {
-                            "text": "dummy_1",
-                            "entity": "dummy_entity_1",
-                            "slotName": "dummy_slotName"
-                        }
-                    ]
-            }
-        ],
-        "dummy_intent_2": [
-            {
-                "data":
-                    [
-                        {
-                            "text": "This is a "
-                        },
-                        {
-                            "text": "dummy_3",
-                            "entity": "dummy_entity_1",
-                            "slotName": "dummy_slotName"
-                        },
-                        {
-                            "text": " query from another intent"
-                        }
-                    ]
-            }
-        ]
-    }
-    return queries
-
-
-def get_entities():
-    entries_1 = [
-        {
-            "value": "dummy_a",
-            "synonyms": ["dummy_a", "dummy 2a", "dummy a", "2 dummy a"]
-        },
-        {
-            "value": "dummy_b",
-            "synonyms": ["dummy_b", "dummy_bb", "dummy b"]
-        },
-        {
-            "value": "dummy\d",
-            "synonyms": ["dummy\d"]
-        },
-    ]
-    entity_1 = Entity("dummy_entity_1", entries=entries_1)
-
-    entries_2 = [
-        {
-            "value": "dummy_c",
-            "synonyms": ["dummy_c", "dummy_cc", "dummy c", "3p.m."]
-        }
-    ]
-    entity_2 = Entity("dummy_entity_2", entries=entries_2)
-    return {entity_1.name: entity_1, entity_2.name: entity_2}
+from snips_nlu.tests.utils import TEST_PATH, SAMPLE_DATASET
 
 
 class TestRegexIntentParser(unittest.TestCase):
@@ -128,8 +12,8 @@ class TestRegexIntentParser(unittest.TestCase):
     _save_path = os.path.join(TEST_PATH, "regex_intent_parser.pkl")
 
     def setUp(self):
-        self._dataset = Dataset(queries=get_queries(),
-                                entities=get_entities())
+        self._dataset = SAMPLE_DATASET
+        validate_dataset(self._dataset)
 
     def tearDown(self):
         if os.path.exists(self._save_path):
@@ -145,29 +29,18 @@ class TestRegexIntentParser(unittest.TestCase):
 
         # Then
         expected_entities = [
-            {
-                "range": (10, 17),
-                "value": "dummy_a",
-                "entity": "dummy_entity_1",
-                "slotName": "dummy_slotName"
-            },
-            {
-                "range": (37, 44),
-                "value": "dummy_c",
-                "entity": "dummy_entity_2",
-                "slotName": "dummy_slotName2"
-            }
+            parsed_entity(match_range=(10, 17), value="dummy_a",
+                          entity="dummy_entity_1", slot_name="dummy_slot_name"),
+            parsed_entity(match_range=(37, 44), value="dummy_c",
+                          entity="dummy_entity_2", slot_name="dummy_slot_name2")
         ]
-        expected_parse = {
-            "text": text,
-            "intent": {"name": "dummy_intent_1",
-                       "prob": (len("dummy_a") + len("dummy_c")) / float(
-                           len(text))},
-            "entities": expected_entities
-        }
-        self.assertEqual(parse["text"], expected_parse["text"])
-        self.assertEqual(parse["intent"], expected_parse["intent"])
-        self.assertItemsEqual(parse["entities"], expected_parse["entities"])
+        expected_proba = (len("dummy_a") + len("dummy_c")) / float(len(text))
+        expected_intent = intent_classification_result(
+            intent_name="dummy_intent_1",
+            probability=expected_proba)
+        expected_parse = result(text=text, parsed_intent=expected_intent,
+                                parsed_entities=expected_entities)
+        self.assertEqual(parse, expected_parse)
 
     def test_should_get_intent(self):
         # Given
@@ -178,10 +51,9 @@ class TestRegexIntentParser(unittest.TestCase):
         intent = parser.get_intent(text)
 
         # Then
-        expected_intent = {
-            "intent": "dummy_intent_1",
-            "prob": (len("dummy_a") + len("dummy_c")) / float(len(text))
-        }
+        probability = (len("dummy_a") + len("dummy_c")) / float(len(text))
+        expected_intent = intent_classification_result(
+            intent_name="dummy_intent_1", probability=probability)
 
         self.assertEqual(intent, expected_intent)
 
@@ -195,31 +67,23 @@ class TestRegexIntentParser(unittest.TestCase):
 
         # Then
         expected_entities = [
-            {
-                "range": (10, 17),
-                "value": "dummy_a",
-                "entity": "dummy_entity_1",
-                "slotName": "dummy_slotName"
-            },
-            {
-                "range": (37, 44),
-                "value": "dummy_c",
-                "entity": "dummy_entity_2",
-                "slotName": "dummy_slotName2"
-            }
+            parsed_entity(match_range=(10, 17), value="dummy_a",
+                          entity="dummy_entity_1", slot_name="dummy_slot_name"),
+            parsed_entity(match_range=(37, 44), value="dummy_c",
+                          entity="dummy_entity_2", slot_name="dummy_slot_name2")
         ]
         self.assertItemsEqual(expected_entities, entities)
 
-    def test_save_and_load(self):
-        # Given
-        parser = RegexIntentParser("dummy_intent_1").fit(self._dataset)
-
-        # When
-        parser.save(self._save_path)
-        new_parser = RegexIntentParser.load(self._save_path)
-
-        # Then
-        self.assertEqual(parser, new_parser)
+        # def test_save_and_load(self):
+        #     # Given
+        #     parser = RegexIntentParser("dummy_intent_1").fit(self._dataset)
+        #
+        #     # When
+        #     parser.save(self._save_path)
+        #     new_parser = RegexIntentParser.load(self._save_path)
+        #
+        #     # Then
+        #     self.assertEqual(parser, new_parser)
 
 
 if __name__ == '__main__':

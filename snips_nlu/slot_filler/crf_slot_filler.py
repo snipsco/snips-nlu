5f6d9274aa3e48312b54e61e16b180e161b62445
Clément Doumouro <clement.doumouro@snips.ai>
2017-03-20 12:12:27 +0100

    sav wwork

--- snips_nlu/slot_filler/crf_slot_filler.py
+++ snips_nlu/slot_filler/crf_slot_filler.py
@@ -2,54 +2,41 @@ import cPickle
 
 from sklearn_crfsuite import CRF
 
-from snips_nlu.slot_filler.crf_feature import CRFFeature
 from snips_nlu.slot_filler.crf_utils import add_bilou_tags, remove_bilou_tags
-from snips_nlu.tokenizer import Tokenizer
 
 
-class CRFSlotFiller:
-    def __init__(self, tokenizer, model, features, use_bilou=True):
-        self._tokenizer = None
-        self.tokenizer = tokenizer
-        self._model = None
-        self.model = model
-        self._features = None
-        self.features = features
+class CRFSlotFiller(object):
+    def __init__(self, crf_model, tokenization_fn, feature_fns,
+                 use_bilou=True):
+        self.tokenization_fn = tokenization_fn
+        self._crf_model = None
+        self.crf_model = crf_model
+        self._feature_fns = None
+        self.feature_fns = feature_fns
         self.use_bilou = use_bilou
         self.fitted = False
 
     @property
-    def tokenizer(self):
-        return self._tokenizer
+    def crf_model(self):
+        return self._crf_model
 
-    @tokenizer.setter
-    def tokenizer(self, value):
-        if not isinstance(value, Tokenizer):
-            raise ValueError("Expected Tokenizer but found: %s"
-                             % type(value))
-        self._tokenizer = value
-
-    @property
-    def model(self):
-        return self._model
-
-    @model.setter
-    def model(self, value):
+    @crf_model.setter
+    def crf_model(self, value):
         if not isinstance(value, CRF):
             raise ValueError("Expected CRF but found: %s" % type(value))
-        self._model = value
+        self._crf_model = value
 
     @property
-    def features(self):
-        return self._features
+    def feature_fns(self):
+        return self._feature_fns
 
-    @features.setter
-    def features(self, value):
-        for feature in value:
-            if not isinstance(feature, CRFFeature):
-                raise ValueError("Expected CRFFeature but found: %s"
-                                 % type(feature))
-        self._features = value
+    @feature_fns.setter
+    def feature_fns(self, value):
+        for feature_fn in value:
+            if not callable(feature_fn):
+                raise ValueError("Expected a callable, got %s"
+                                 % type(feature_fn))
+        self._feature_fns = value
 
     def get_slots(self, text):
         pass
@@ -58,13 +45,13 @@ class CRFSlotFiller:
         if not self.fitted:
             raise AssertionError("Model must be fitted before using predict")
         features = self.compute_features(tokens)
-        predicted_labels = self.model.predict_single(features)
+        predicted_labels = self.crf_model.predict_single(features)
         return remove_bilou_tags(predicted_labels)
 
     def fit(self, data):
         X = [self.compute_features(sample['tokens']) for sample in data]
         Y = [self.add_bilou(sample['labels']) for sample in data]
-        self.model.fit(X, Y)
+        self.crf_model.fit(X, Y)
         self.fitted = True
 
     def add_bilou(self, labels):
@@ -77,12 +64,13 @@ class CRFSlotFiller:
         features = []
         for token in tokens:
             token_features = dict()
-            for feature in self.features:
+            for feature in self.feature_fns:
                 token_features.update({feature.name: feature.compute(token)})
             features.append(token_features)
         return features
 
     def save(self, filepath):
+        # TODO: dump as json + use PyCRFSuite serialization
         cPickle.dump(self, open(filepath, 'wb'))
 
     @classmethod
---
+++ snips_nlu/token.py
@@ -0,0 +1,3 @@
+from collections import namedtuple
+
+Token = namedtuple('Token', 'value', 'start', 'end')

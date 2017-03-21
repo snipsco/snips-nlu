11f5421a67f7d64f1d374697380cb905b79eeac2
Adrien Ball <adrien.ball@snips.net>
2017-03-21 17:30:16 +0100

    Clean SlotFiller

--- snips_nlu/slot_filler/crf_slot_filler.py
+++ snips_nlu/slot_filler/crf_slot_filler.py
@@ -1,52 +1,23 @@
-import cPickle
-
-from sklearn_crfsuite import CRF
-
-from snips_nlu.slot_filler.crf_utils import add_bilou_tags, remove_bilou_tags
+from snips_nlu.slot_filler.crf_utils import add_bilou_tags, remove_bilou_tags, \
+    OUTSIDE
 
 
 class CRFSlotFiller(object):
-    def __init__(self, crf_model, tokenization_fn, feature_fns,
-                 use_bilou=True):
-        self.tokenization_fn = tokenization_fn
-        self._crf_model = None
+    def __init__(self, crf_model, features, use_bilou=True):
         self.crf_model = crf_model
-        self._feature_fns = None
-        self.feature_fns = feature_fns
+        self.features = features
         self.use_bilou = use_bilou
         self.fitted = False
 
-    @property
-    def crf_model(self):
-        return self._crf_model
-
-    @crf_model.setter
-    def crf_model(self, value):
-        if not isinstance(value, CRF):
-            raise ValueError("Expected CRF but found: %s" % type(value))
-        self._crf_model = value
-
-    @property
-    def feature_fns(self):
-        return self._feature_fns
-
-    @feature_fns.setter
-    def feature_fns(self, value):
-        for feature_fn in value:
-            if not callable(feature_fn):
-                raise ValueError("Expected a callable, got %s"
-                                 % type(feature_fn))
-        self._feature_fns = value
-
-    def get_slots(self, text):
-        pass
-
-    def predict(self, tokens):
+    def get_slots(self, tokens):
         if not self.fitted:
             raise AssertionError("Model must be fitted before using predict")
         features = self.compute_features(tokens)
         predicted_labels = self.crf_model.predict_single(features)
-        return remove_bilou_tags(predicted_labels)
+        predicted_labels = self.remove_bilou(predicted_labels)
+        predicted_labels = map(lambda l: l if l != OUTSIDE else None,
+                               predicted_labels)
+        return predicted_labels
 
     def fit(self, data):
         X = [self.compute_features(sample['tokens']) for sample in data]
@@ -61,18 +32,12 @@ class CRFSlotFiller(object):
         return remove_bilou_tags(labels) if self.use_bilou else labels
 
     def compute_features(self, tokens):
+        cache = [{"token": token} for token in tokens]
         features = []
-        for token in tokens:
+        for i in range(len(tokens)):
             token_features = dict()
-            for feature in self.feature_fns:
-                token_features.update({feature.name: feature.compute(token)})
+            for feature in self.features:
+                value = feature.compute(token_index=i, cache=cache)
+                token_features.update({feature.name: value})
             features.append(token_features)
         return features
-
-    def save(self, filepath):
-        # TODO: dump as json + use PyCRFSuite serialization
-        cPickle.dump(self, open(filepath, 'wb'))
-
-    @classmethod
-    def load(cls, filepath):
-        return cPickle.load(filepath)

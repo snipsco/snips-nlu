cea122b0b88f3744094a61849073b2e69be11d3e
Adrien Ball <adrien.ball@snips.net>
2017-03-22 14:39:24 +0100

    Remove nlu_engine package

--- snips_nlu/nlu_engine.py
+++ snips_nlu/nlu_engine.py
@@ -15,16 +15,23 @@ class NLUEngine(object):
         raise NotImplementedError
 
 
-def _parse(text, parsers):
+def _parse(text, parsers, threshold):
     if len(parsers) == 0:
         return Result(text, parsed_intent=None, parsed_entities=None)
+
+    best_parser = None
+    best_intent = None
     for parser in parsers:
         res = parser.get_intent(text)
         if res is None:
             continue
-        entities = parser.get_entities(text, res.intent_name)
-        return Result(text, parsed_intent=res, parsed_entities=entities)
-    return Result(text, parsed_intent=None, parsed_entities=None)
+        if best_intent is None or res.probability > best_intent.probability:
+            best_intent = res
+            best_parser = parser
+    if best_intent is None or best_intent.probability <= threshold:
+        return Result(text, parsed_intent=None, parsed_entities=None)
+    entities = best_parser.get_entities(text, best_intent.intent_name)
+    return Result(text, parsed_intent=best_intent, parsed_entities=entities)
 
 
 class SnipsNLUEngine(NLUEngine):
@@ -34,22 +41,34 @@ class SnipsNLUEngine(NLUEngine):
             custom_parsers = []
         self.custom_parsers = custom_parsers
         self.builtin_parser = builtin_parser
+        self.fitted = False
 
     def parse(self, text):
-        parsers = self.custom_parsers
-        if self.builtin_parser is not None:
-            parsers.append(self.builtin_parser)
-        return _parse(text, parsers)
+        custom_parse = _parse(text, self.custom_parsers, threshold=0.)
+        if custom_parse.parsed_intent is not None:
+            return custom_parse
+        elif self.builtin_parser is not None:
+            return _parse(text, [self.builtin_parser], threshold=0.)
+        else:
+            return Result(text=text, parsed_intent=None, parsed_entities=None)
 
     def fit(self, dataset):
         validate_dataset(dataset)
-        custom_parser = RegexIntentParser().fit(dataset)
-        self.custom_parsers = [custom_parser]
+        updated_parsers = []
+        for intent_name in dataset["intents"].keys():
+            parser = RegexIntentParser(intent_name).fit(dataset)
+            updated_parsers.append(parser)
+        self.custom_parsers = updated_parsers
+        self.fitted = True
         return self
 
     def save_to_pickle_string(self):
         self.builtin_parser = None
         return cPickle.dumps(self)
+
+    @classmethod
+    def load_from_dict(cls, obj_dict):
+        return SnipsNLUEngine()
 
     @classmethod
     def load_from_pickle_and_path(cls, pkl_str, builtin_path):

8bdb9a333492b9b426208360cb0c61b216d0f642
Clément Doumouro <clement.doumouro@snips.ai>
2017-03-23 18:51:58 +0100

    Rename token to tokenization to avoid import problem and implement tokenization

---
+++ snips_nlu/tokenization.py
@@ -0,0 +1,11 @@
+import re
+from collections import namedtuple
+
+Token = namedtuple('Token', 'value', 'start', 'end')
+
+TOKEN_REGEX = re.compile(r"\w+", re.UNICODE)
+
+
+def tokenize(string):
+    return [Token(m.group(), m.start(), m.end())
+            for m in TOKEN_REGEX.finditer(string)]

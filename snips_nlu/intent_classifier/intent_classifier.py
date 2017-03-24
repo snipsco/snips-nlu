960acd14d59103db3ba7e79304edbdf4024518f3
Adrien Ball <adrien.ball@snips.net>
2017-03-24 14:30:58 +0100

    Fix intent classifier

--- snips_nlu/intent_classifier/intent_classifier.py
+++ snips_nlu/intent_classifier/intent_classifier.py
@@ -1,9 +1,13 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, abstractproperty
 from snips_nlu.result import IntentClassificationResult
 
 
 class IntentClassifier(object):
     __metaclass__ = ABCMeta
+
+    @abstractproperty
+    def fitted(self):
+        pass
 
     @abstractmethod
     def fit(self, dataset):
@@ -18,10 +22,15 @@ class SnipsIntentClassifier(IntentClassifier):
     def __init__(self):
         self.intent_name = None
 
+    @property
+    def fitted(self):
+        return self.intent_name is not None
+
     def fit(self, dataset):
         intents = dataset["intents"]
         if len(intents.keys()) > 0:
             self.intent_name = intents.keys()[0]
+        return self
 
     def get_intent(self, text):
         if self.intent_name is not None:

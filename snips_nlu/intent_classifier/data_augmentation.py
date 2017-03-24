b7d21e5b4ab67db2c75686910b55402252b50d75
Alice <alice.coucke@gmail.com>
2017-03-24 19:14:03 +0100

    fix for new dataset format

--- snips_nlu/intent_classifier/data_augmentation.py
+++ snips_nlu/intent_classifier/data_augmentation.py
@@ -4,11 +4,11 @@ from intent_classifier_resources import get_subtitles
 
 def augment_dataset(dataset, language='en'):
 
-	intent_list = dataset.queries.keys()
+	intent_list = dataset['intents'].keys()
 	intent_code = dict((intent, i+1) for i,intent in enumerate(intent_list))
 
 	# get size of smaller intent (for noise amount and regularization)
-	queries_per_intent = [ len(dataset.queries[intent]) for intent in dataset.queries ]
+	queries_per_intent = [ len(dataset['intents'][intent]) for intent in intent_list ]
 	mean_queries_per_intent = np.mean(queries_per_intent)
 	alpha = 1.0/(4*(sum(queries_per_intent)+5*mean_queries_per_intent))
 
@@ -22,8 +22,8 @@ def augment_dataset(dataset, language='en'):
 	'intent_val': []
 	}
 	for intent in intent_list:
-		queries += [ ''.join( [ dataset.queries[intent][i]['data'][j]['text'] for j in range(len(dataset.queries[intent][i]['data'])) ] ) for i in range(len(dataset.queries[intent])) ]
-		y +=  [ intent_code[intent] for _ in xrange(len(dataset.queries[intent])) ]
+		queries += [ ''.join( [ dataset['intents'][intent]['utterances'][i]['data'][j]['text'] for j in range(len(dataset['intents'][intent]['utterances'][i]['data'])) ] ) for i in range(len(dataset['intents'][intent]['utterances'])) ]
+		y +=  [ intent_code[intent] for _ in xrange(len(dataset['intents'][intent]['utterances'])) ]
 
 	queries += [ ''.join( [ queries_noise[i]['data'][j]['text'] for j in range(len(queries_noise[i]['data'])) ] ) for i in range(len(queries_noise)) ]
 	y += [ 0 for _ in len(queries_noise) ]

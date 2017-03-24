fa5f4800b804dc4a3fcdaf42a47749ddea152744
Adrien Ball <adrien.ball@snips.net>
2017-03-24 11:04:32 +0100

    Add API documentation

--- snips_nlu/nlu_engine.py
+++ snips_nlu/nlu_engine.py
@@ -37,12 +37,23 @@ class SnipsNLUEngine(NLUEngine):
         self.builtin_parser = builtin_parser
 
     def parse(self, text):
+        """
+        Parse the input text and returns a dictionary containing the most
+        likely intent and slots.
+        """
         parsers = self.custom_parsers
         if self.builtin_parser is not None:
             parsers.append(self.builtin_parser)
         return _parse(text, parsers)
 
     def fit(self, dataset):
+        """
+        Fit the engine with a dataset
+        :param dataset: A dictionary containing the data of the custom intents.
+        See https://github.com/snipsco/snips-nlu/blob/develop/README.md for
+        details about the format.
+        :return: A fitted SnipsNLUEngine
+        """
         validate_dataset(dataset)
         custom_parser = RegexIntentParser().fit(dataset)
         intent_classifier = None
@@ -52,17 +63,33 @@ class SnipsNLUEngine(NLUEngine):
         return self
 
     def save_to_pickle_string(self):
+        """
+        Serialize the SnipsNLUEngine to a pickle string, after having reset the
+        builtin intent parser. Thus this serialization, contains only the
+        custom intent parsers.
+        """
         self.builtin_parser = None
         return cPickle.dumps(self)
 
     @classmethod
     def load_from_pickle_and_path(cls, pkl_str, builtin_path):
+        """
+        :param pkl_str: content of the pickle file describing the nlu engine
+        :param builtin_path: path of the builtin intent parser directory
+        :return: a SnipsNLUEngine already fitted
+        """
         engine = cPickle.loads(pkl_str)
         engine.builtin_parser = BuiltinIntentParser(builtin_path)
         return engine
 
     @classmethod
     def load_from_pickle_and_byte_array(cls, pkl_str, builtin_byte_array):
+        """
+        :param pkl_str: content of the pickle file describing the nlu engine
+        :param builtin_byte_array: byte_array data need to initialize the
+        builtin intent parser
+        :return:
+        """
         engine = cPickle.loads(pkl_str)
         # TODO: update engine with builtin parsers using builtin_byte_array
         return engine

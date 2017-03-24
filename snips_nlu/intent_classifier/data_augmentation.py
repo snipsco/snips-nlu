fea8bca87351b947b1ba91e6602772ffc7023ccb
Alice <alice.coucke@gmail.com>
2017-03-24 17:54:31 +0100

    fix indent

--- snips_nlu/intent_classifier/data_augmentation.py
+++ snips_nlu/intent_classifier/data_augmentation.py
@@ -4,15 +4,15 @@ from intent_classifier_resources import get_subtitles
 
 def augment_dataset(dataset, language='en'):
 
-	intent_list = dataset.queries.keys()
-	intent_code = dict((intent, i+1) for i,intent in enumerate(intent_list))
+    intent_list = dataset.queries.keys()
+    intent_code = dict((intent, i+1) for i,intent in enumerate(intent_list))
 
-	# get size of smaller intent (for noise amount and regularization)
-	queries_per_intent = [ len(dataset.queries[intent]) for intent in dataset.queries ]
-	mean_queries_per_intent = np.mean(queries_per_intent)
-	alpha = 1.0/(4*(sum(queries_per_intent)+5*mean_queries_per_intent)
-	
-	data_noise_train = get_subtitles("en")
+    # get size of smaller intent (for noise amount and regularization)
+    queries_per_intent = [ len(dataset.queries[intent]) for intent in dataset.queries ]
+    mean_queries_per_intent = np.mean(queries_per_intent)
+    alpha = 1.0/(4*(sum(queries_per_intent)+5*mean_queries_per_intent)
+    
+    data_noise_train = get_subtitles("en")
     queries_noise = np.random.choice(data_noise_train, size=5*mean_queries_per_intent, replace=False)
 
     queries = []
@@ -22,13 +22,13 @@ def augment_dataset(dataset, language='en'):
     'intent_val': []
     }
     for intent in intent_list:
-    	queries += [ ''.join( [ dataset.queries[intent][i]['data'][j]['text'] for j in range(len(dataset.queries[intent][i]['data'])) ] ) for i in range(len(dataset.queries[intent])) ]
-    	y +=  [ intent_code[intent] for _ in xrange(len(dataset.queries[intent])) ]
+        queries += [ ''.join( [ dataset.queries[intent][i]['data'][j]['text'] for j in range(len(dataset.queries[intent][i]['data'])) ] ) for i in range(len(dataset.queries[intent])) ]
+        y +=  [ intent_code[intent] for _ in xrange(len(dataset.queries[intent])) ]
 
- 	queries += [ ''.join( [ queries_noise[i]['data'][j]['text'] for j in range(len(queries_noise[i]['data'])) ] ) for i in range(len(queries_noise)) ]
+    queries += [ ''.join( [ queries_noise[i]['data'][j]['text'] for j in range(len(queries_noise[i]['data'])) ] ) for i in range(len(queries_noise)) ]
     y += [ 0 for _ in len(queries_noise) ]
 
     queries = np.array(queries)
     y = np.array(y)
     
-	return (queries, y), alpha, intent_list
+    return (queries, y), alpha, intent_list

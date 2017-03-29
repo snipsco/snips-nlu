3f468bda3052bce36eff8bdf4ee456976bf9b183
Clément Doumouro <clement.doumouro@snips.ai>
2017-03-29 14:17:18 +0200

    Refactor built-in entities

--- snips_nlu/built_in_entities.py
+++ snips_nlu/built_in_entities.py
@@ -1,7 +1,7 @@
 from duckling import core
 from enum import Enum
 
-from utils import LimitedSizeDict
+from utils import LimitedSizeDict, classproperty
 
 core.load()
 
@@ -11,27 +11,64 @@ class BuiltInEntity(Enum):
     DURATION = {"label": "snips/duration", "duckling_dim": "duration"}
     NUMBER = {"label": "snips/number", "duckling_dim": "number"}
 
+    def __init__(self, value):
+        Enum.__init__(value)
+        enum_class = type(self)
+        try:
+            enum_class._built_in_entity_by_label[self.label] = self
+        except AttributeError:
+            enum_class._built_in_entity_by_label = dict()
+        enum_class._built_in_entity_by_label[self.label] = self
 
-DUCKLING_DIM_TO_SNIPS_ENTITY = dict((ent.value["duckling_dim"], ent)
-                                    for ent in BuiltInEntity)
-BUILT_ENTITY_BY_LABEL = dict((ent.value["label"], ent)
-                             for ent in BuiltInEntity)
+        try:
+            enum_class._built_in_entity_by_duckling_dim[
+                self.duckling_dim] = self
+        except AttributeError:
+            enum_class._built_in_entity_by_duckling_dim = dict()
+        enum_class._built_in_entity_by_duckling_dim[self.duckling_dim] = self
 
+    @property
+    def label(self):
+        return self.value["label"]
 
-class BuiltInEntityLookupError(LookupError):
-    pass
+    @property
+    def duckling_dim(self):
+        return self.value["duckling_dim"]
 
+    @classproperty
+    @classmethod
+    def built_in_entity_by_label(cls):
+        return cls._built_in_entity_by_label
 
-def get_built_in_entity_by_label(label):
-    try:
-        ent = BUILT_ENTITY_BY_LABEL[label]
-    except:
-        raise BuiltInEntityLookupError("Unknown entity %s" % label)
-    return ent
+    @classmethod
+    def get_from_label(cls, label, default=None):
+        try:
+            cls.built_in_entity_by_label[label]
+        except KeyError:
+            if default is None:
+                raise KeyError("Unknown entity '%s'" % label)
+            else:
+                return default
+
+    @classproperty
+    @classmethod
+    def built_in_entity_by_duckling_dim(cls):
+        return cls._built_in_entity_by_duckling_dim
+
+    @classmethod
+    def get_from_duckling_dim(cls, duckling_dim, default=None):
+
+        try:
+            cls.built_in_entity_by_duckling_dim[duckling_dim]
+        except KeyError:
+            if default is None:
+                raise KeyError("Unknown duckling dim '%s'" % duckling_dim)
+            else:
+                return default
 
 
 def scope_to_dims(scope):
-    return [entity.value["duckling_dim"] for entity in scope]
+    return [entity.duckling_dim for entity in scope]
 
 
 _DUCKLING_CACHE = LimitedSizeDict(size_limit=1000)
@@ -56,14 +93,7 @@ def get_built_in_entities(text, language, scope=None):
             parsed_entity = {
                 "match_range": (ent["start"], ent["end"]),
                 "value": ent["body"],
-                "entity": DUCKLING_DIM_TO_SNIPS_ENTITY[ent["dim"]]
+                "entity": BuiltInEntity.from_dim([ent["dim"]])
             }
             parsed_entities.append(parsed_entity)
     return parsed_entities
-
-
-if __name__ == '__main__':
-    texts = ["be there at 2p.m. please", "we go there at 2 is it OK"]
-    for t in texts:
-        entities = get_built_in_entities(t, "en", scope=[])
-        print t, entities

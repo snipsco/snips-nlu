e320cbabfdb285e6331c2964b4a0a7bf43048c5b
Adrien Ball <adrien.ball@snips.net>
2017-03-29 10:20:43 +0200

    Fix path

--- snips_nlu/tests/utils.py
+++ snips_nlu/tests/utils.py
@@ -5,10 +5,11 @@ import os
 from snips_nlu.dataset import validate_dataset
 
 TEST_PATH = os.path.dirname(os.path.abspath(__file__))
+SAMPLE_DATASET_PATH = os.path.join(TEST_PATH, "resources/sample_dataset.json")
 
 EMPTY_DATASET = {"intents": {}, "entities": {}}
 
-with io.open("resources/sample_dataset.json") as dataset_file:
+with io.open(SAMPLE_DATASET_PATH) as dataset_file:
     SAMPLE_DATASET = json.load(dataset_file)
 
 validate_dataset(EMPTY_DATASET)

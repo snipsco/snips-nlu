752c66806a614ed3f6907273615fc52746f2aa93
Alice <alice.coucke@gmail.com>
2017-03-30 15:54:02 +0200

    PEP8

--- snips_nlu/nlu_engine.py
+++ snips_nlu/nlu_engine.py
@@ -1,19 +1,18 @@
-import cPickle
 from abc import ABCMeta, abstractmethod
 
-from snips_nlu.built_in_entities import BuiltInEntity
 from dataset import validate_dataset
+from snips_nlu.built_in_entities import BuiltInEntity
 from snips_nlu.constants import (
-    USE_SYNONYMS, SYNONYMS, DATA, INTENTS, ENTITIES, SLOT_NAME, UTTERANCES,
+    USE_SYNONYMS, SYNONYMS, DATA, INTENTS, ENTITIES, UTTERANCES,
     LANGUAGE, VALUE, AUTOMATICALLY_EXTENSIBLE, ENTITY)
 from snips_nlu.intent_classifier.snips_intent_classifier import \
     SnipsIntentClassifier
 from snips_nlu.intent_parser.builtin_intent_parser import BuiltinIntentParser
 from snips_nlu.intent_parser.crf_intent_parser import CRFIntentParser
+from snips_nlu.intent_parser.regex_intent_parser import RegexIntentParser
 from snips_nlu.languages import Language
 from snips_nlu.result import ParsedSlot
 from snips_nlu.result import Result
-from snips_nlu.intent_parser.regex_intent_parser import RegexIntentParser
 from snips_nlu.slot_filler.crf_tagger import CRFTagger, default_crf_model
 from snips_nlu.slot_filler.crf_utils import Tagging
 from snips_nlu.slot_filler.feature_functions import crf_features

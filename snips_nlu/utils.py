be586023bf4529559ad10388b6cf01672c635599
Adrien Ball <adrien.ball@snips.net>
2017-03-30 18:02:16 +0200

    Update Token tuple

--- snips_nlu/utils.py
+++ snips_nlu/utils.py
@@ -1,6 +1,6 @@
 import importlib
 import os
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple, Mapping
 
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 RESOURCES_PATH = os.path.join(ROOT_PATH, "snips_nlu", "snips-nlu-resources")
@@ -118,6 +118,17 @@ class UnupdatableDict(dict):
         if key in self:
             raise KeyError("Can't update key '%s'" % key)
         super(UnupdatableDict, self).__setitem__(key, value)
+
+
+def namedtuple_with_defaults(typename, field_names, default_values=()):
+    T = namedtuple(typename, field_names)
+    T.__new__.__defaults__ = (None,) * len(T._fields)
+    if isinstance(default_values, Mapping):
+        prototype = T(**default_values)
+    else:
+        prototype = T(*default_values)
+    T.__new__.__defaults__ = tuple(prototype)
+    return T
 
 
 def get_resources_path(language):

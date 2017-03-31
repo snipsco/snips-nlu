a0043338d95f645aac9ee3a518523866b81e85e4
Adrien Ball <adrien.ball@snips.net>
2017-03-31 15:00:18 +0200

    Bump duckling dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name="snips_nlu",
       download_url="",
       license="MIT",
       install_requires=[
-          "duckling==0.0.8",
+          "duckling==0.0.11",
           "pytest",
           "enum34",
           "mock",

8fe09a5f131164d6ac4f6881558824967d145b30
Adrien Ball <adrien.ball@snips.net>
2017-03-31 15:55:29 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
+import os
+
 from setuptools import setup, find_packages
+from snips_nlu.languages import Language
 
 packages = find_packages()
+language_resources = [os.path.join("snips-nlu-resources", l.iso_code, "*") for l
+                      in Language]
 
 setup(name="snips_nlu",
       version="0.0.3",
@@ -22,7 +27,7 @@ setup(name="snips_nlu",
           "snips-queries==0.1.0a1"
       ],
       packages=packages,
-      package_data={"": ["snips-nlu-resources/*", "tests/resources/*"]},
+      package_data={"": language_resources + ["tests/resources/*"]},
       entry_points={},
-      include_package_data=False,
+      include_package_data=True,
       zip_safe=False)

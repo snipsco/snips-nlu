67d1e75e43b9894316af8e3edd53d11edbffcf13
Alice <alice.coucke@gmail.com>
2017-04-07 14:27:17 +0200

    point to last version of duckling

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name="snips_nlu",
       download_url="",
       license="MIT",
       install_requires=[
-          "duckling==0.0.13",
+          "duckling==0.0.14",
           "pytest",
           "enum34",
           "mock",

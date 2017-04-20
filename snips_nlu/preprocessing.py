0169c45795b7f35ac6d6038537eab683fce3f0e3
Adrien Ball <adrien.ball@snips.net>
2017-04-20 18:37:24 +0200

    Remove verbs_stems function

--- snips_nlu/preprocessing.py
+++ snips_nlu/preprocessing.py
@@ -7,23 +7,6 @@ from snips_nlu.utils import RESOURCES_PATH
 _LANGUAGE_STEMS = dict()
 
 
-def verbs_stems(language):
-    stems_paths = glob.glob(os.path.join(RESOURCES_PATH, language.iso_code,
-                                         "top_*_verbs_conjugated.txt"))
-    if len(stems_paths) == 0:
-        return dict()
-
-    verb_stemmings = dict()
-    with io.open(stems_paths[0], encoding="utf8") as f:
-        lines = [l.strip() for l in f]
-    for line in lines:
-        elements = line.split(';')
-        verb_stemmings.update(
-            {inflection.split(',')[1]: elements[0] for inflection in
-             elements[1:]})
-    return verb_stemmings
-
-
 def verbs_lexemes(language):
     stems_paths = glob.glob(os.path.join(RESOURCES_PATH, language.iso_code,
                                          "top_*_verbs_lexemes.txt"))

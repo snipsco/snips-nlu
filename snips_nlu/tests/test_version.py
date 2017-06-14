1ba19dcd5ed99c179107b215766220792541ddc4
Adrien Ball <adrien.ball@snips.net>
2017-06-14 18:18:15 +0200

    Add __model_version__

--- snips_nlu/tests/test_version.py
+++ snips_nlu/tests/test_version.py
@@ -2,7 +2,7 @@ import unittest
 
 from semantic_version import Version
 
-from snips_nlu import __version__
+from snips_nlu import __version__, __model_version__
 
 
 class TestVersion(unittest.TestCase):
@@ -14,6 +14,21 @@ class TestVersion(unittest.TestCase):
         valid = False
         try:
             Version(version)
+            valid = True
+        except ValueError:
+            pass
+
+        # Then
+        self.assertTrue(valid, "Version number '%s' is not semantically valid")
+
+    def model_version_should_be_semantic(self):
+        # Given
+        model_version = __model_version__
+
+        # When
+        valid = False
+        try:
+            Version(model_version)
             valid = True
         except ValueError:
             pass

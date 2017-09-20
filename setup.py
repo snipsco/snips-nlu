11778465b87dcacdf2ed4eb9eebc1e5257d6467d
Adrien Ball <adrien.ball@snips.net>
2017-09-20 19:13:33 +0200

    Bump rustling to 7.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ required = [
     "sklearn-crfsuite==0.3.5",
     "builtin_entities_ontology==0.4.2",
     "semantic_version==2.6.0",
-    "rustling==6.1",
+    "rustling==7.0",
     "nlu_utils==0.2.0"
 ]
 

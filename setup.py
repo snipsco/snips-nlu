dfed4187ee9557be8e7aa7b30c1761e18254a374
Adrien Ball <adrien.ball@snips.net>
2018-01-16 19:51:29 +0100

    Bump dependencies

--- setup.py
+++ setup.py
@@ -20,10 +20,10 @@ required = [
     "scipy==0.19.0",
     "scikit-learn==0.18.1",
     "sklearn-crfsuite==0.3.5",
-    "builtin_entities_ontology==0.5.1",
+    "builtin_entities_ontology==0.5.2",
     "semantic_version==2.6.0",
-    "rustling==8.3",
-    "nlu_utils==0.5.0",
+    "rustling==8.4.0",
+    "nlu_utils==0.5.2",
     "num2words==0.5.5"
 ]
 
@@ -32,7 +32,7 @@ extras_require = {
         "mock==2.0.0",
         "nlu_metrics==0.10.0",
         "snips_nlu_rust==0.51.16",
-        "pylint==1.7.4",
+        "pylint==1.8.1",
         "coverage==4.4.1"
     ]
 }

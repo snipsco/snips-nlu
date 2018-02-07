879d7a7e04f0bc1e865f3a8b255f077e2f976425
Adrien Ball <adrien.ball@snips.net>
2018-02-07 20:28:15 +0100

    Add intersphinx mapping for sklearn_crfsuite

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -299,4 +299,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'https://docs.python.org/': None,
+    'https://sklearn-crfsuite.readthedocs.io/en/stable/': None
+}

aa4b2b36f59a18b26be8e992fcf8d0e68b9ef287
Adrien Ball <adrien.ball@snips.net>
2018-02-13 14:29:27 +0100

    Skip integration test temporarily

--- snips_nlu/tests/integration_test.py
+++ snips_nlu/tests/integration_test.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 import unittest
+from unittest import skip
 
 from future.utils import iteritems
 from nlu_metrics import (compute_cross_val_metrics,
@@ -32,6 +33,7 @@ class IntegrationTestSnipsNLUEngine(unittest.TestCase):
         # Then
         self.check_metrics(results)
 
+    @skip("Skip this test until crf tags are fixed in snips-nlu-rs")
     def test_python_rust_engine_performance(self):
         # Given
         dataset_path = PERFORMANCE_DATASET_PATH

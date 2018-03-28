d9b86765ce2bd84237b0fad6ac29b0968811e88c
ClemDoum <clement.doumouro@gmail.com>
2018-03-28 16:42:08 +0200

    Remove LimitedSizeDict

--- snips_nlu/tests/test_utils.py
+++ snips_nlu/tests/test_utils.py
@@ -1,57 +1,5 @@
-from future.utils import iteritems
-
 from snips_nlu.tests.utils import SnipsTest
-from snips_nlu.utils import LimitedSizeDict, ranges_overlap
-
-
-class TestLimitedSizeDict(SnipsTest):
-    def test_should_raise_when_no_size_limit(self):
-        # Given/When/Then
-        with self.assertRaises(ValueError) as ctx:
-            LimitedSizeDict()
-        self.assertEqual(str(ctx.exception.args[0]),
-                         "'size_limit' must be passed as a keyword argument")
-
-    def test_should_initialize_with_argument(self):
-        # Given
-        sequence = [("a", 1), ("b", 2)]
-        size_limit = 3
-        # When
-        d = LimitedSizeDict(sequence, size_limit=size_limit)
-        # Then
-        items = sorted(iteritems(d), key=lambda i: i[0])
-        self.assertListEqual(items, sequence)
-
-    def test_should_initialize_without_argument(self):
-        # Given
-        size_limit = 10
-        # When
-        d = LimitedSizeDict(size_limit=size_limit)
-        # Then
-        self.assertListEqual(list(d), [])
-
-    def test_should_wrong_when_initialization_should_raise_error(self):
-        # Given
-        sequence = [("a", 1), ("b", 2), ("c", 3)]
-        size_limit = len(sequence) - 1
-        # When/Then
-        with self.assertRaises(ValueError) as ctx:
-            LimitedSizeDict(sequence, size_limit=size_limit)
-        self.assertEqual(str(ctx.exception.args[0]),
-                         "Tried to initialize LimitedSizedDict with more "
-                         "value than permitted with 'limit_size'")
-
-    def test_should_erase_items_when_updating(self):
-        # Given
-        sequence = [("a", 1), ("b", 2), ("c", 3), ("d", 4)]
-        size_limit = len(sequence) - 2
-        # When
-        my_dict = LimitedSizeDict(sequence[:size_limit], size_limit=size_limit)
-        for k, v in sequence[size_limit:]:
-            my_dict[k] = v
-        # Then
-        items = sorted(list(iteritems(my_dict)), key=lambda i: i[0])
-        self.assertListEqual(items, sequence[size_limit:])
+from snips_nlu.utils import ranges_overlap
 
 
 class TestUtils(SnipsTest):
--- snips_nlu/utils.py
+++ snips_nlu/utils.py
@@ -3,10 +3,10 @@ from __future__ import unicode_literals
 import errno
 import numbers
 import os
-from builtins import object
-from collections import OrderedDict, namedtuple, Mapping
+from collections import namedtuple, Mapping
 
 import numpy as np
+from builtins import object
 
 from snips_nlu.constants import (INTENTS, UTTERANCES, DATA, SLOT_NAME, ENTITY,
                                  RESOURCES_PATH, END, START)
@@ -82,34 +82,6 @@ def validate_range(rng):
     if not isinstance(rng, (list, tuple)) or len(rng) != 2 or rng[0] > rng[1]:
         raise ValueError("range must be a length 2 list or tuple and must be "
                          "valid")
-
-
-class LimitedSizeDict(OrderedDict):
-    def __init__(self, *args, **kwds):
-        if "size_limit" not in kwds:
-            raise ValueError("'size_limit' must be passed as a keyword "
-                             "argument")
-        self.size_limit = kwds.pop("size_limit")
-        if len(args) > 1:
-            raise TypeError('expected at most 1 arguments, got %d' % len(args))
-        if len(args) == 1 and len(args[0]) + len(kwds) > self.size_limit:
-            raise ValueError("Tried to initialize LimitedSizedDict with more "
-                             "value than permitted with 'limit_size'")
-        super(LimitedSizeDict, self).__init__(*args, **kwds)
-
-    def __setitem__(self, key, value, dict_setitem=OrderedDict.__setitem__):
-        dict_setitem(self, key, value)
-        self._check_size_limit()
-
-    def _check_size_limit(self):
-        if self.size_limit is not None:
-            while len(self) > self.size_limit:
-                self.popitem(last=False)
-
-    def __eq__(self, other):
-        if self.size_limit != other.size_limit:
-            return False
-        return super(LimitedSizeDict, self).__eq__(other)
 
 
 class UnupdatableDict(dict):

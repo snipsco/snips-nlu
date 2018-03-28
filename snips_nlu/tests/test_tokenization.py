5ba1a75daff4a020ec3c2aa0839650426aaf1c7f
ClemDoum <clement.doumouro@gmail.com>
2018-03-28 19:38:05 +0200

    Remove useless test

--- snips_nlu/tests/test_tokenization.py
+++ snips_nlu/tests/test_tokenization.py
@@ -5,7 +5,7 @@ from snips_nlu_ontology import get_all_languages
 
 from snips_nlu.constants import LANGUAGE_EN
 from snips_nlu.tests.utils import SnipsTest
-from snips_nlu.tokenization import tokenize, Token, RegexTokenizer
+from snips_nlu.tokenization import tokenize, Token
 
 
 class TestTokenization(SnipsTest):
@@ -71,31 +71,3 @@ class TestTokenization(SnipsTest):
             tokens = tokenize(text, l)
             # Then
             self.assertEqual(len(tokens), 0)
-
-    def test_regex_tokenize(self):
-        # Given
-        pattern = "\w+"
-        tokenizer = RegexTokenizer(pattern)
-        text = "the orange is in da_kitchen, I repeat: " \
-               "the ban!ana is in da bedroom"
-        # When
-        tokens = tokenizer.tokenize(text)
-
-        # Then
-        expected_tokens = [
-            Token("the", 0, 3),
-            Token("orange", 4, 10),
-            Token("is", 11, 13),
-            Token("in", 14, 16),
-            Token("da_kitchen", 17, 27),
-            Token("I", 29, 30),
-            Token("repeat", 31, 37),
-            Token("the", 39, 42),
-            Token("ban", 43, 46),
-            Token("ana", 47, 50),
-            Token("is", 51, 53),
-            Token("in", 54, 56),
-            Token("da", 57, 59),
-            Token("bedroom", 60, 67)
-        ]
-        self.assertSequenceEqual(expected_tokens, tokens)

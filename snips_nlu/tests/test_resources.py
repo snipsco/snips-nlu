00fe15730cfe6a7a23db7736c4a5f60cb0af5dfc
Adrien Ball <adrien.ball@snips.net>
2018-06-21 13:48:47 +0200

    Add tests for resources

--- snips_nlu/tests/test_resources.py
+++ snips_nlu/tests/test_resources.py
@@ -4,10 +4,51 @@ import unittest
 
 from mock import patch
 
-from snips_nlu.resources import _get_resource, MissingResource
+from snips_nlu.constants import DATA_PATH
+from snips_nlu.resources import _get_resource, MissingResource, \
+    clear_resources, load_resources, resource_exists
 
 
 class TestResources(unittest.TestCase):
+    def test_should_load_resources_from_data_path(self):
+        # Given
+        clear_resources()
+
+        # When
+        load_resources("en")
+
+        # Then
+        self.assertTrue(resource_exists("en", "gazetteers"))
+
+    def test_should_load_resources_from_package(self):
+        # Given
+        clear_resources()
+
+        # When
+        load_resources("snips_nlu_en")
+
+        # Then
+        self.assertTrue(resource_exists("en", "gazetteers"))
+
+    def test_should_load_resources_from_path(self):
+        # Given
+        clear_resources()
+        resources_path = DATA_PATH / "en"
+
+        # When
+        load_resources(str(resources_path))
+
+        # Then
+        self.assertTrue(resource_exists("en", "gazetteers"))
+
+    def test_should_fail_loading_unknown_resources(self):
+        # Given
+        unknown_resource_name = "foobar"
+
+        # When / Then
+        with self.assertRaises(MissingResource):
+            load_resources(unknown_resource_name)
+
     def test_should_raise_missing_resource_when_language_not_found(self):
         # Given
         mocked_value = dict()

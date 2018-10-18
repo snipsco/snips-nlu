01048cec72c18a9373db9536fef67ece13ff12a3
Adrien Ball <adrien.ball@snips.ai>
2018-10-18 13:50:41 +0200

    Add optional verbose mode in training command

--- snips_nlu/cli/training.py
+++ snips_nlu/cli/training.py
@@ -1,20 +1,26 @@
-from __future__ import unicode_literals, print_function
+from __future__ import print_function, unicode_literals
 
 import json
-from pathlib import Path
+import logging
 
 import plac
+from pathlib import Path
 
-from snips_nlu import load_resources, SnipsNLUEngine
+from snips_nlu import SnipsNLUEngine, load_resources
+from snips_nlu.cli.utils import set_nlu_logger
 
 
 @plac.annotations(
     dataset_path=("Path to the training dataset file", "positional", None,
                   str),
     output_path=("Path of the output model", "positional", None, str),
-    config_path=("Path to the NLU engine configuration", "option", "c", str))
-def train(dataset_path, output_path, config_path):
+    config_path=("Path to the NLU engine configuration", "option", "c", str),
+    verbose=("Print logs", "flag", "v"),
+)
+def train(dataset_path, output_path, config_path, verbose):
     """Train an NLU engine on the provided dataset"""
+    if verbose:
+        set_nlu_logger(logging.INFO)
     with Path(dataset_path).open("r", encoding="utf8") as f:
         dataset = json.load(f)
 

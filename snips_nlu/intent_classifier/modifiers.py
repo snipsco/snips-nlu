4f338c665771f73cf0d77cf7909b448800950de4
Adrien Ball <adrien.ball@snips.ai>
2018-11-26 17:49:11 +0100

    Update Changelog

---
+++ snips_nlu/intent_classifier/modifiers.py
@@ -0,0 +1,24 @@
+MODIFIERS = {
+    "it": {
+        "più",
+        "piu",
+        "meno",
+        "molto",
+        "non",
+        "troppo",
+        "troppa",
+        "ancora",
+        "senza",
+        "con",
+        "forte",
+        "forti",
+        "alto",
+        "alta",
+        "alti",
+        "alte"
+        "bassa",
+        "basso",
+        "bassi",
+        "basse"
+    }
+}

faf6fe12116188fea92bcb2ddc125bf2217daa4a
ClemDoum <clement.doumouro@gmail.com>
2019-07-11 17:51:47 +0200

    Bump version to 0.20.0

--- snips_nlu/__about__.py
+++ snips_nlu/__about__.py
@@ -13,8 +13,8 @@ __author__ = "Clement Doumouro, Adrien Ball"
 __email__ = "clement.doumouro@snips.ai, adrien.ball@snips.ai"
 __license__ = "Apache License, Version 2.0"
 
-__version__ = "0.19.8"
-__model_version__ = "0.19.0"
+__version__ = "0.20.0"
+__model_version__ = "0.20.0"
 
 __download_url__ = "https://github.com/snipsco/snips-nlu-language-resources/releases/download"
 __compatibility__ = "https://raw.githubusercontent.com/snipsco/snips-nlu-language-resources/master/compatibility.json"
